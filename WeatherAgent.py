694fc598c49d3681e526d82031247d0620ebd78b
Seth Haslam <sethhaslam3@gmail.com>
2025-09-24 11:06:10 +0100

    Added message streaming for more understandable output

--- WeatherAgent.py
+++ WeatherAgent.py
@@ -6,8 +6,8 @@ from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 load_dotenv()
 
+# Adding memory
 
-memory = MemorySaver()
 
 model = ChatOpenAI(model="gpt-3.5-turbo")
 
@@ -16,16 +16,24 @@ model = ChatOpenAI(model="gpt-3.5-turbo")
 search = TavilySearch(max_results=2)
 
 tools=[search]
-
+# Adding memory checkpointer
 agent_executor = create_react_agent(model, tools)
 
 model_with_tools = model.bind_tools(tools)
 
 
-
+# Asking the user for the location they would like to know the weather in
 location = input("Please enter the your location to find out the weather: ")
 input_message = {"role": "user", "content": "Search for the weather in " + location}
+
+# Calling the agent with the message and location
 response = agent_executor.invoke({"messages": [input_message]})
 
-for message in response["messages"]:
-    message.pretty_print()
+# # Using langsmith Trace to see whats going on under the hood
+# for message in response["messages"]:
+#     message.pretty_print()
+
+# Streaming Messages
+for step in agent_executor.stream({"messages": [input_message]}, stream_mode="values"):
+    step["messages"][-1].pretty_print()
+    
